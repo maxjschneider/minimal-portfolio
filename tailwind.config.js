e3639acb9681ff4853b3589102b98d9f675304b2
Max Schneider <maxwjs03@gmail.com>
2024-09-08 14:21:26 -0400

    initial upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['src/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"), 
+    require('daisyui')
+  ],
+}
+
